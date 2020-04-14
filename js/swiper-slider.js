d3a92f5a2c0677772b79a3e7a57c366f594f181e
Tetiana <tetiana_prykhidko@ukr.net>
2020-04-14 22:51:50 +0300

    fix swiper

---
+++ js/swiper-slider.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	let swiper = new Swiper('.swiper-container', {
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'progressbar',
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
+})
+
+
